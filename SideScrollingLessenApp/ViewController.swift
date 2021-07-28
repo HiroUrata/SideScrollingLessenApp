5b5496b594f2d7c4d98c0bd6f7a1fb2b15672170
HiroUrata <1712026@i.softbank.jp>
2021-07-28 18:48:54 +0900

    Initial Commit

---
+++ SideScrollingLessenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideScrollingLessenApp
+//
+//  Created by UrataHiroki on 2021/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
